51afcd64879ae7d9228c162a6c73e935f37b9c27
TVNL CHARAN <vnlcharan.t@gmail.com>
2024-07-01 16:07:33 +0530

    fixed resource section

--- src/components/SoundHook/useClickSound.jsx
+++ src/components/SoundHook/useClickSound.jsx
@@ -1,22 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 const useClickSound = (soundFile, excludedSelectors = []) => {
   useEffect(() => {
     const clickSound = new Audio(soundFile);
-    clickSound.preload = 'auto'; // Ensure audio file is preloaded
-    clickSound.volume=0.3;
+    clickSound.preload = "auto";
+    clickSound.volume = 0.1;
 
     const handleClick = (event) => {
-      if (excludedSelectors.some(selector => event.target.closest(selector))) {
+      if (
+        excludedSelectors.some((selector) => event.target.closest(selector))
+      ) {
         return;
       }
-      clickSound.currentTime = 0; // Rewind sound to start
+      clickSound.currentTime = 0;
       clickSound.play();
     };
 
-    document.addEventListener('click', handleClick);
+    document.addEventListener("click", handleClick);
     return () => {
-      document.removeEventListener('click', handleClick);
+      document.removeEventListener("click", handleClick);
     };
   }, [soundFile, excludedSelectors]);
 };
