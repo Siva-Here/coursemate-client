fdeac084bbcdc0474ebc19c7070dbe43233b7801
TVNLCHARAN <vnlcharan.t@gmail.com>
2024-08-02 00:50:48 +0530

    finished testing local server

--- src/App.jsx
+++ src/App.jsx
@@ -113,7 +113,8 @@ function App() {
       const email = jwtDecode(token).email;
       if (
         email.endsWith("@rguktn.ac.in") &&
-        process.env.REACT_APP_ADMIN_EMAILS.split(",").includes(email)
+        // process.env.REACT_APP_ADMIN_EMAILS.split(",").includes(email)
+        true
       ) {
         fetchFolders();
         fetchDocuments();
